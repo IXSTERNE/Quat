e59a219acdd5af3e24e0fa1286a367bf874e773f
IXSTERNE <ixsterne@gmail.com>
2024-06-26 23:40:50 +0800

    +texts

--- projection.py
+++ projection.py
@@ -15,20 +15,28 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 scale = 100
 circle_pos = [WIDTH / 2, HEIGHT / 2]
 points = []
+point_texts = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
-points.append(np.matrix([-1, -1, 1]))
-points.append(np.matrix([1, -1, 1]))
-points.append(np.matrix([1, 1, 1]))
-points.append(np.matrix([-1, 1, 1]))
-points.append(np.matrix([-1, -1, -1]))
-points.append(np.matrix([1, -1, -1]))
-points.append(np.matrix([1, 1, -1]))
-points.append(np.matrix([-1, 1, -1]))
+points.append(np.matrix([-1, -1, 1]).reshape((3, 1)))
+points.append(np.matrix([1, -1, 1]).reshape((3, 1)))
+points.append(np.matrix([1, 1, 1]).reshape((3, 1)))
+points.append(np.matrix([-1, 1, 1]).reshape((3, 1)))
+points.append(np.matrix([-1, -1, -1]).reshape((3, 1)))
+points.append(np.matrix([1, -1, -1]).reshape((3, 1)))
+points.append(np.matrix([1, 1, -1]).reshape((3, 1)))
+points.append(np.matrix([-1, 1, -1]).reshape((3, 1)))
 
 projection_matrix = np.matrix([
     [1, 0, 0],
     [0, 1, 0]
 ])
+
+text_font = pygame.font.SysFont("Arial", 15)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
 
 while True:
     for event in pygame.event.get():
@@ -42,11 +50,11 @@ while True:
     
     screen.fill(WHITE)
 
-    for point in points:
-        projected2d = np.dot(projection_matrix, point.reshape((3, 1)))
-        print(projected2d)
-        x = int(projected2d[0][0] * scale) + circle_pos[0]
-        y = int(projected2d[1][0] * scale) + circle_pos[1]
+    for point, text in zip(points, point_texts):
+        projected2d = np.dot(projection_matrix, point)
+        x = int(projected2d[0, 0] * scale) + circle_pos[0]
+        y = int(projected2d[1, 0] * scale) + circle_pos[1]
+        draw_text(text, text_font, BLACK, x + 5, y + 5)
         pygame.draw.circle(screen, BLACK, (x, y), 5)
 
     pygame.display.update()
