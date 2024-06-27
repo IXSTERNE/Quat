aaa7ed6874877ee4f3a85cb6ca5dff174ca33674
IXSTERNE <ixsterne@gmail.com>
2024-06-27 21:07:45 +0800

    hmm

--- projection.py
+++ projection.py
@@ -10,24 +10,15 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WIDTH, HEIGHT = 800, 600
 
-pygame.display.set_caption("3D projection")
+pygame.display.set_caption("Quaternion 3D rotation in 2D projection")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-scale = 100
 circle_pos = [WIDTH / 2, HEIGHT / 2]
 points = []
-angle = 0
 point_texts = ["A", "B", "C", "D", "E", "F", "G", "H"]
 text_font = pygame.font.SysFont("Arial", 15)
+angle = 0
 
-points.append(np.matrix([-1, -1, 1]).reshape((3, 1)))
-points.append(np.matrix([1, -1, 1]).reshape((3, 1)))
-points.append(np.matrix([1, 1, 1]).reshape((3, 1)))
-points.append(np.matrix([-1, 1, 1]).reshape((3, 1)))
-points.append(np.matrix([-1, -1, -1]).reshape((3, 1)))
-points.append(np.matrix([1, -1, -1]).reshape((3, 1)))
-points.append(np.matrix([1, 1, -1]).reshape((3, 1)))
-points.append(np.matrix([-1, 1, -1]).reshape((3, 1)))
+
 
 vertices = [[-1, -1, 1], [1, -1, 1], 
             [1, 1, 1], [-1, 1, 1],
@@ -40,9 +31,7 @@ edges = [[0, 1], [1, 2], [2, 3], [3, 0],
 
 
 
-
-
-def rotate_vectors(sample_vertices):
+def rotate_vectors(sample_vertices, q, q_con):
 
     rotated_vectors = []
     
@@ -60,13 +49,10 @@ def project_points(sample_rotated_vectors):
     
     for point in sample_rotated_vectors:
         x, y, z = point
-        f =  200 / (z + 5)
+        f = 200 / (z + 4)
         x, y = x * f, y * f
-        projected_points.append([WIDTH / 2 + int(x), HEIGHT / 2 - int(y)])
+        projected_points.append([WIDTH / 2 + x, HEIGHT / 2 - y])
     return projected_points
-
-
-
 
 
 def draw_text(text, font, text_col, x, y):
@@ -75,46 +61,35 @@ def draw_text(text, font, text_col, x, y):
 
 
 
-clock = pygame.time.Clock()
+
 while True:
 
-    clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_ESCAPE:
-                pygame.quit()
-                exit()
 
 
-# Update Stuff
+    angle += 2
 
-    q = Quaternion.compute_rotation(angle, 1, 1, 0)
+    q = Quaternion.compute_rotation(math.radians(angle), 1, 1, 0)
     q_con = q.conjugate()
 
-
-    rotated_vectors = rotate_vectors(vertices)
+    rotated_vectors = rotate_vectors(vertices, q, q_con)
     projected_points = project_points(rotated_vectors)
     
-
-    angle += 1
-
-    screen.fill(WHITE)
+    screen.fill(BLACK)
 
     for point, text in zip(projected_points, point_texts):
-        x = point[0]
-        y = point[1]
-        draw_text(text, text_font, BLACK, x + 5, y + 5)
-        pygame.draw.circle(screen, BLACK, (x, y), 5)
-
-
+        x, y = point[0], point[1]
+        draw_text(text, text_font, WHITE, x + 5, y + 5)
+        pygame.draw.circle(screen, WHITE, (x, y), 5)
+        
+    
     for edge in edges:
         p1, p2 = edge
         pygame.draw.line(screen, RED, projected_points[p1], projected_points[p2], 1)
     
     
-
-    pygame.display.update()
+    pygame.display.flip()
