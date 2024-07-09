e53712cfb4d3f75a42e9360e33cd8cdb895d0318
IXSTERNE <ixsterne@gmail.com>
2024-07-10 01:23:38 +0800

    what do

--- projection.py
+++ projection.py
@@ -1,7 +1,6 @@
 import pygame
 from ternion import Ternion
 from slerp import slerp
-import numpy as np
 import math
 
 pygame.init()
@@ -18,22 +17,23 @@ origin = [WIDTH / 2, HEIGHT / 2]
 point_texts = ["A", "B", "C", "D", "E", "F", "G", "H"]
 text_font = pygame.font.SysFont("Arial", 15)
 angle = 0
-degrees = 360
+degrees = 90
 
-vec_x = 2
+vec_x = 1
 vec_y = 1
-vec_z = 6
+vec_z = 0
+
+checkpoint = None
 
 
-vertices = np.array([
-            [-1, -1, 1], 
+vertices = [[-1, -1, 1], 
             [1, -1, 1], 
             [1, 1, 1], 
             [-1, 1, 1],
             [-1, -1, -1], 
             [1, -1, -1], 
             [1, 1, -1], 
-            [-1, 1, -1]])
+            [-1, 1, -1]]
 
 edges = [[0, 1], [1, 2], [2, 3], [3, 0],
          [4, 5], [5, 6], [6, 7], [7, 4],
@@ -47,7 +47,7 @@ def rotate_vectors(sample_vertices, q):
     for vertex in sample_vertices:
         pure_quaternion = Ternion.pure_quaternion(vertex[0], vertex[1], vertex[2])
         rotated_vertices = q * pure_quaternion * q.conjugate()
-        rotated_vector = [rotated_vertices.x, rotated_vertices.y, rotated_vertices.z]
+        rotated_vector = [0, rotated_vertices.x, rotated_vertices.y, rotated_vertices.z]
         rotated_vectors.append(rotated_vector)
     return rotated_vectors
 
@@ -57,8 +57,9 @@ def project_points(sample_rotated_vectors):
     projected_points = []
     
     for point in sample_rotated_vectors:
-        x, y, z = point
-
+        x = point[1]
+        y = point[2]
+        z = point[3]
         f = 600 / (z + 5)
         x, y = x * f, y * f
 
@@ -82,26 +83,27 @@ while True:
             exit()
 
     if angle <= degrees:
-        angle += 0.07
+        angle += 0.03
 
 
     q = Ternion.compute_rotation(angle, vec_x, vec_y, vec_z)
     
     rotated_vectors = rotate_vectors(vertices, q)
+
+    if checkpoint is not None:
+
+        interpolated_vectors = slerp(checkpoint, rotated_vectors, 0.5)
+
+        print(interpolated_vectors)
+
+        # for point, text in zip(interpolated_vectors, point_texts):
+        #     x, y = point[0], point[1]
+        #     pygame.draw.circle(screen, WHITE, (x, y), 5)
+        #     draw_text(text, text_font, WHITE, x + 5, y + 5)
+
     projected_points = project_points(rotated_vectors)
         
     screen.fill(BLACK)
-
-    for point, text in zip(projected_points, point_texts):
-        x, y = point[0], point[1]
-        pygame.draw.circle(screen, WHITE, (x, y), 5)
-        draw_text(text, text_font, WHITE, x + 5, y + 5)
-    
-    
-    for edge in edges:
-        p1, p2 = edge
-        pygame.draw.line(screen, RED, projected_points[p1], projected_points[p2], 1)
-
 
     angle_text = "Angle: {}".format(int(angle))
     draw_text(angle_text, text_font, WHITE, 10, 10)
@@ -117,5 +119,18 @@ while True:
     draw_text(q_text_i, text_font, WHITE, 10, 40)
     draw_text(q_text_j, text_font, WHITE, 10, 55)
     draw_text(q_text_k, text_font, WHITE, 10, 70)
+    draw_text("O", text_font, WHITE, WIDTH / 2, HEIGHT / 2)
+
+    
+
+    for point, text in zip(projected_points, point_texts):
+        x, y = point[0], point[1]
+        pygame.draw.circle(screen, WHITE, (x, y), 5)
+        draw_text(text, text_font, WHITE, x + 5, y + 5)
+
+
+    for edge in edges:
+        p1, p2 = edge
+        pygame.draw.line(screen, RED, projected_points[p1], projected_points[p2], 1)
 
     pygame.display.update()
